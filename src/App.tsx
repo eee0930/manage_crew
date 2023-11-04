053a6e0fece384dee9dd1bc9ffb61d4680de55c2
lazyhysong <eee0930@naver.com>
2023-11-04 20:13:27 +0900

    feat: handsontable 모듈 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,18 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Handsontable from 'handsontable/base';
+import { registerAllModules } from 'handsontable/registry';
+import {
+  registerCellType,
+  DateCellType,
+  HandsontableCellType,
+  NumericCellType,
+  TextCellType,
+} from 'handsontable/cellTypes';
+import 'handsontable/dist/handsontable.full.min.css';
+
+registerAllModules();
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <div className="App"></div>;
 }
 
 export default App;
