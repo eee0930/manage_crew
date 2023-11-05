cae63f70b9e3181f57af3db9ab22dc0467855695
lazyhysong <eee0930@naver.com>
2023-11-05 20:49:16 +0900

    feat: context menu 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,42 @@
 import Handsontable from 'handsontable/base';
 import { registerAllModules } from 'handsontable/registry';
 import { HotTable } from '@handsontable/react';
+import { Settings } from 'handsontable/plugins/contextMenu';
 import './App.css';
 import 'handsontable/dist/handsontable.full.min.css';
 import Button from './components/Button';
 import { DEFAULT_DATA } from './default_data';
 
 registerAllModules();
+
 const HOTTABLE_PROPS = {
   height: 'auto',
   width: 'auto',
   licenseKey: 'non-commercial-and-evaluation',
 };
+
+const CONTEXT_MENU: Settings = [
+  'row_above',
+  'row_below',
+  '---------',
+  'col_left',
+  'col_right',
+  '---------',
+  'remove_col',
+  'remove_row',
+];
+
 function App() {
   const handleClickSave = () => {};
   return (
     <div className="App">
       <Button callback={handleClickSave}>저장</Button>
-      <HotTable data={DEFAULT_DATA} {...HOTTABLE_PROPS} />
+      <HotTable
+        data={DEFAULT_DATA.slice(1)}
+        colHeaders={DEFAULT_DATA[0] as string[]}
+        contextMenu={CONTEXT_MENU}
+        {...HOTTABLE_PROPS}
+      />
     </div>
   );
 }
